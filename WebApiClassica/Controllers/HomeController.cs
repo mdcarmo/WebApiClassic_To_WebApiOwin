f94aa6bf0588a93f683fc6fe395f7be27a1c8e3d
mdcarmo <marc29dias@yahoo.com.br>
2017-07-19 13:52:32 -0300

    inclusão inicial

---
+++ WebApiClassica/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiClassica.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World WEB API Clássica";
+        }
+    }
+}
---
+++ WebApiOwin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiOwin.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World WEB API OWIN";
+        }
+    }
+}
