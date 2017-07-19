73215f226d73fb1fb158d07587043e86da236d4b
mdcarmo <marc29dias@yahoo.com.br>
2017-07-19 15:41:29 -0300

    alterações

---
+++ WebApiOwin/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+//[assembly: OwinStartup(typeof(WebApiOwin.Startup))]
+
+namespace WebApiOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //configuração WebApi
+            var config = new HttpConfiguration();
+
+            //configurando rotas
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+            );
+
+            // ativando configuração WebApi
+            app.UseWebApi(config);
+        }
+    }
+}
